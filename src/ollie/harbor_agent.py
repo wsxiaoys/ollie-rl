dd1bbca3ede7aa98cfa82b013f6dccb03bf82710
Meng Zhang <meng@tabbyml.com>
2026-08-03 12:12:02 +0800

    fix: update default version to 0.4.1

--- src/ollie/harbor_agent.py
+++ src/ollie/harbor_agent.py
@@ -35,7 +35,7 @@ from ollie.harbor_environment import OllieEnvironment
 class OllieAgent(BaseInstalledAgent):
     """Run Ollie using the required :class:`OllieEnvironment` adapter."""
 
-    _DEFAULT_VERSION = "0.4.0"
+    _DEFAULT_VERSION = "0.4.1"
     _OUTPUT_FILENAME = "ollie.ndjson"
     _STDERR_FILENAME = "ollie.stderr"
 
